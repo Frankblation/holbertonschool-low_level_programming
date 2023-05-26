dac2964da38d41ad1242f912bd7fe48cd9a1c550
Frankblation <frankblation@gmail.com>
2023-05-26 01:24:39 -0700

    lesgo

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 *function ( : ) - puts program is like building
 *return:- value of 'main (....)
 */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
