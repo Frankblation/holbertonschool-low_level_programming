5610a40d544a29c8b12ef659e3a8dd402b83d982
Frankblation <frankblation@gmail.com>
2023-05-27 08:55:19 -0700

    yasss

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *function ( : ) - puts program is like building
- *return: - value of 'main remained zero(....)
-*/
+ *my_function - main value is zero
+ *Description: puts with a return value of (0)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
