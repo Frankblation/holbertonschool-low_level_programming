cdb6aa1966cd21bb423f4eb9706d4df33c2a3ded
Frankblation <frankblation@gmail.com>
2023-05-27 09:33:14 -0700

    sage

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * my_function - printf
+ * Descrition: this is the code printing out text
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art, \n);
+	return (0);
+}
