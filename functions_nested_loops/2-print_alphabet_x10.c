d7533922e33b30e08c9bd95342766ee0032d3d34
Frankblation <frankblation@gmail.com>
2023-05-30 22:39:22 -0700

    do it

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,17 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char letter;
+	char bet;
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (bet = 'a'; bet <= 'z'; bet++)
+				_putchar(bet);
+	}
+
+	{
 		_putchar('\n');
+
+		bet++;
 	}
 }
