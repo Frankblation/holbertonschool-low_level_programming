b63946808edf36de763ed6db663abf6b095152b1
Frankblation <frankblation@gmail.com>
2023-05-31 09:12:16 -0700

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - shows absolute value of integer
+ * @n: computing value of integer
+ * Return: Always 0
+ */
+
+int_abs(int n)
+{
+	if (n < 0)
+	return (-n);
+
+	else if (n > 0)
+	return (n);
+
+	else
+
+	return (0);
+}
