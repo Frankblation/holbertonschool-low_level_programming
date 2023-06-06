a871fb332ef0c8a982ae532d8313dc179a3021c5
Frankblation <frankblation@gmail.com>
2023-06-05 17:02:44 -0700

    mostly

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9 using the _putchar function
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 9; x++)
+
+		if (x != 2 && x != 4)
+
+			putchar(x + '0');
+	putchar('\n');
+}
