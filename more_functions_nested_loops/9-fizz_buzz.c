f659d16fc6799e0aa75de53d5e32295b5cfa8b96
Frankblation <frankblation@gmail.com>
2023-06-05 21:39:23 -0700

    done

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints Fizz Buzz
+ * Return: Always ® (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1 x <= 100; x++)
+	{
+		if ((× % 3 == 0) && (× % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (× % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (× % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", ×);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		printf("\n");
+		return (0);
+}
