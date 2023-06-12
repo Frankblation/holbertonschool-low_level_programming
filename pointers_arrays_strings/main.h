ae44b231424f6d92f392bb9f5f4a41159df230ca
Frankblation <frankblation@gmail.com>
2023-06-12 11:38:48 -0700

    hitit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,28 +4,22 @@
 #include <unistd.h>
 /*This is the document for all the prototypes*/
 
-void reset_to_98(int *n);
+char *_memset(char *s, char b, unsigned int n);
 
-void swap_int(int *a, int *b);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
-int _strlen(char *s);
+char *_strchr(char *s, char c);
 
-void _puts(char *str);
+unsigned int _strspn(char *s, char *accept);
 
-void print_rev(char *s);
+char *_strpbrk(char *s, char *accept);
 
-void rev_string(char *s);
+char *_strstr(char *haystack, char *needle);
 
-void puts2(char *str);
+void print_chessboard(char (*a)[8]);
 
-void puts_half(char *str);
+void print_diagsums(int *a, int size);
 
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
-
-int _putchar(char c);
+int _putchar(char c)
 
 #endif
