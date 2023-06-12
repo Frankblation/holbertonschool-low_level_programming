a8a5c718859ad814f558f16efe83a1f696c0d007
Frankblation <frankblation@gmail.com>
2023-06-12 02:09:50 -0700

    not even

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - main is void
- *
+ * reset_to_98 - main is void
+ * @n: the value
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
-       
-	*n = 98;
+{
 
-		return (0);
+	*n = 98;
 }
