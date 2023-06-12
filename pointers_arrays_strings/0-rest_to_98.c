9b7b14fbc29a05ccb82a3071d1cad6cb3b7a459b
Frankblation <frankblation@gmail.com>
2023-06-12 00:43:15 -0700

    maybe

---
+++ pointers_arrays_strings/0-rest_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main is void
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 402;
+
+	int *p = &n;
+
+	*p = 98;
+
+		return (0);
+}
