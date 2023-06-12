7fd87712b56a6b9d6c5443bead293da92a2404d1
Frankblation <frankblation@gmail.com>
2023-06-12 12:34:37 -0700

    no

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,24 +4,18 @@
  * @s: buffer array
  * @b: constant byte
  * @n: number of bytes of memory area to fill
- * Description: Fill the first `n` bytes of the memory area pointed to by `s`
- * with the constant byte `b`.
- * Return: Pointer to memory area `s`
+ * Description: Fill the first n bytes of the memory area pointed to by s
+ * with the constant byte b
+ * Return: Pointer to memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n);
 
 {
 
-	int i;
+	unsigned int i;
 
-	i = 0;
-	while (n > 0)
-	{
-		s[i] = b;
-		i++;
-		n--;
-	}
-
+	for (i = 0; i < n; i++)
+		s[1] = b;
 	return (s);
 }
