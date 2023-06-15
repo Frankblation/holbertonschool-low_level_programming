052beba3f9634e38dcea374594587d53790e0c46
Frankblation <frankblation@gmail.com>
2023-06-15 13:04:28 -0700

    hit

--- recursion/main.h
+++ recursion/main.h
@@ -1,1 +1,10 @@
-2a0366de3216d8e98042
+#ifndef HEADER_H                                                                                        
+#define HEADER_H                                                                                        
+#include <stdio.h>
+#include <unistd.h>
+/*This is the document for all the prototypes*/
+
+void _puts_recursion(char *s);
+
+int _putchar(char c);
+#endif
