f4b75930b0f6d2d380481eaaf5ed49156d890997
Frankblation <frankblation@gmail.com>
2023-06-16 10:28:30 -0700

    star

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Define the length
+ * 
+ * Return: Always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0
+
+	if (*s == '\0')
+		return 0;
+	else 
+		return 1 + n(str + 1);
+}
