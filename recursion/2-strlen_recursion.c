5f93826bb4014c6835cd1c508666e1112363d204
Frankblation <frankblation@gmail.com>
2023-06-18 12:20:33 -0700

    yes

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
  * _strlen_recursion - Define the length
- * 
+ * @s: The pointer of string
  * Return: Always 0
  */
 
 int _strlen_recursion(char *s)
 {
-	int n = 0
-
-	if (*s == '\0')
-		return 0;
-	else 
-		return 1 + n(str + 1);
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
