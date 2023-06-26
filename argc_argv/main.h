eb6cf255312bad14a115911c6d4d4247543d2766
Frankblation <frankblation@gmail.com>
2023-06-25 23:28:58 -0700

    yew

---
+++ argc_argv/main.h
@@ -0,0 +1,9 @@
+#define HEADER_H                                                                                        
+#include <stdio.h>
+#include <unistd.h>
+/*This is the document for all the prototypes*/
+
+int main(int argc, char **argv);
+
+int _putchar(char c);
+#endif
