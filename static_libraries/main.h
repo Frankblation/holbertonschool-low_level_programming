7c05ddcbb63b30d32087d64139b8e8623af7144e
Frankblation <frankblation@gmail.com>
2023-06-26 21:35:42 -0700

    yes

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_H                                                                                        
-#define HEADER_H                                                                                        
+#ifndef HEADER_H
+#define HEADER_H
 #include <stdio.h>
 #include <unistd.h>
 /*This is the document for all the prototypes*/
