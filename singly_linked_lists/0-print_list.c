6f9f1c0a78bb8759569ef4c0335632f51e12c5a1
Frankblation <frankblation@gmail.com>
2023-07-14 16:44:17 -0700

    1st hit in termminal

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,16 +1,21 @@
 #include "lists.h"
-
+/**
+ * print_list - list the elements in a linked list
+ * @h: h is the head of the list that starts the count.
+ *
+ * Return: the return of the count of elements.
+ */
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
+	const list_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-         prinf("%s\n"), current->str);
-         count++;
-         current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%s\n", current->str);
+		count++;
+		current = current->next;
+	}
 
-    return count;
+	return (count);
 }
