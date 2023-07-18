4572e9571279e50caabd9da5451729e56219e14e
Frankblation <frankblation@gmail.com>
2023-07-17 22:01:00 -0700

    i guess

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,32 @@
 #include "lists.h"
-/**
- * free_list - function with one argument
- * @head: pointer to list_t
- *
- * Description: frees a list
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	list_t *move;
 
-	while (head != NULL)
+/**
+ * add_nodeint_end - function with two arguments
+ * @head: double pointer to head of first linked list
+ * @n: integer value of data in node
+ *
+ * Description: add a new node at the end of linked list
+ * Return: address of new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp, *cursor;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->next = NULL;
+	temp->n = n;
+
+	if (*head)
 	{
-		move = head->next;
-		free(head->str);
-		free(head);
-		head = move;
+		cursor = *head;
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = temp;
 	}
+	else
+		*head = temp;
+	return (temp);
 }
