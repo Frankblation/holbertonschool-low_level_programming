12a460d8ef1ea709a002fd4ebda96de36f674897
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-17 22:32:04 -0500

    comiit 2/3

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -10,7 +10,12 @@
 
 unsigned int binary_to_uint(const char *b);
 
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+
 int _putchar(char c);
 
 char *_strdup(const char *str);
+
 #endif
