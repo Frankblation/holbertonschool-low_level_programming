b74c6b10119e938ea5feb117f540eb2ecd692822
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-21 03:35:57 -0500

    ;

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -6,7 +6,7 @@
 *
 * Return: The number of nodes traversed throuh by trav.
 */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 
 	const dlistint_t *trav = h;
