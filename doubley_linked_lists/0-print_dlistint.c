3f907cf54476ccf4421871345bbf19b243f20ed9
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-20 22:25:53 -0500

    lets go

---
+++ doubley_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#inlude "lists.h"
+
+/**
+* print_dlistint - Prints all elements in dlistint
+* @h: h is pointing to the head node.
+*
+* Return: The number of nodes traversed throuh by trav.
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t  *trav = h;
+    size_t count = 0;
+
+    while(trav != NULL)
+    {
+        printf("%d/n", trav->n);
+        trav = trav->next;
+        node_count++;
+    }
+
+    return (node_count);
+}
---
+++ doubley_linked_lists/README.md
@@ -0,0 +1,5 @@
+This is a readme for doubly linked list.
+pointers that point in multiple directions.
+you move the Int through the nodes until you hit null
+just to  return the  count of int.
+to move to the next using the the pointer.
