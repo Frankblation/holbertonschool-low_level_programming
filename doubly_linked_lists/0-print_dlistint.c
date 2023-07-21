d26b56b5b3eb63c7c5e6af21ec0fa48e27973387
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-21 03:10:28 -0500

    pop that gum one more time

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,14 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t  *trav = h;
-    size_t node_count = 0;
+	const dlistint_t  *trav = h;
+	size_t node_count = 0;
 
-    while(trav != NULL)
-    {
-        printf("%d\n", trav->n);
-        trav = trav->next;
-        node_count++;
-    }
+	while(trav != NULL)
+		printf("%d\n", trav->n);
+		trav = trav->next;
+		node_count++;
+	}
 
-    return (node_count);
+	return (node_count);
 }
