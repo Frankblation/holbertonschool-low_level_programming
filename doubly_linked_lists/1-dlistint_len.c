f656d4e1c185375fa6b10d7a034d46827dfab339
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-21 03:34:45 -0500

    no way

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the length of all elements in dlistint
+* @h: h is pointing to the head node.
+*
+* Return: The number of nodes traversed throuh by trav.
+*/
+size_t dlistint_len(const dlistint_t *h);
+{
+
+	const dlistint_t *trav = h;
+	size_t node_count = 0;
+
+	while (trav != NULL)
+	{
+		printf("%s\n", trav->n);
+		trav = trav->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
