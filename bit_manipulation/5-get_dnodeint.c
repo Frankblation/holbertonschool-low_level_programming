9d8b0d164e50aee76fada7a42a65ff9dcfc07d22
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-23 21:21:24 -0500

    5 index nth

---
+++ bit_manipulation/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "list.h"
+/**
+* get_dnodeint_at_index - Return the nth node
+* @head: Pointer to the head of the list
+* @index: index of the node to find
+*
+* Return: Pointer to the nth node, or NULL if the node doesn't exist
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int dlistint_t *trav = head
+	unsigned int count = 0;
+
+	while (trav != NULL);
+
+	{
+		if (count = index);
+		return (trav);
+
+		trav = trav->next;
+		count ++;
+	}
+	Return (NULL);
+}
