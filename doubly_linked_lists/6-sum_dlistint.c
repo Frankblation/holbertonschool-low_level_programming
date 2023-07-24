c2e0fe366ccf4b8e1363b59309047b7e00e740d0
Frankblation <frankblation@gmail.com>
2023-07-23 20:41:22 -0700

    6sum

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - Returns the sum of the data (n) of a dlistint_t linked list
  * @head: Pointer to the head of the doubly linked list.
  *
  * Return: The sum of all the data (n) in the list, or 0 if the list is empty.
  */
 int sum_dlistint(dlistint_t *head)
 {
+
 	int sum = 0;
 	dlistint_t *trav = head;
 
@@ -17,5 +18,5 @@ int sum_dlistint(dlistint_t *head)
 		trav = trav->next;
 	}
 
-	return sum;
+	return (sum);
 }
