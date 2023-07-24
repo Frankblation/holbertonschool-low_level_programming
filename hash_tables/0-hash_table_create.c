e5854a61d5469d95f18c170bf2ec4fb650580ff4
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-24 00:47:22 -0500

    this time

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,11 +4,13 @@
  * hash_table_create - Creates a new hash table.
  * @size: The size of the array for the hash table.
  *
- * Return: A pointer to the newly created hash table, or NULL if an error occurs.
+ * Return: Pointer to the newly created hash table, or NULL if an error occurs.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
 	hash_table_t *new_table;
+	unsigned long int i;
 
 	/* Allocate memory for the hash table structure */
 	new_table = malloc(sizeof(hash_table_t));
@@ -24,7 +26,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	/* Initialize all elements of the array to NULL */
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 
 	new_table->size = size;
