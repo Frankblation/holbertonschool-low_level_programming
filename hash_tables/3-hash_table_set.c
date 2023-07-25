3bea3f5a68a966765fe89d6139fb55e0be597e81
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-24 23:29:21 -0500

    maybe

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-<<<<<<< HEAD
+
     if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
         return (0);
 
@@ -52,46 +52,3 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
     return (1);
 }
-=======
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-		return (0);
-
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current_node = ht->array[index];
-
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			free(current_node->value);
-			current_node->value = strdup(value);
-			return (current_node->value == NULL ? 0 : 1);
-		}
-		current_node = current_node->next;
-	}
-
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-	return (1);
-}
->>>>>>> f8fc3d38b6c6e59921f62b2142cbbca25f87408b
