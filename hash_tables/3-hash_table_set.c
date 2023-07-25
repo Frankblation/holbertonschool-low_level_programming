9c135647052f57db3cc9fdad5d5e5bd9b2920e82
Frankblation <frankblation@gmail.com>
2023-07-24 21:11:38 -0700

    get

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <string.h>
 
 /**
  * hash_table_set - Adds an element to the hash table.
@@ -11,55 +10,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current_node;
+    if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+        return (0);
 
-	/* Check if the hash table, key, or value is NULL */
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-		return (0);
+    unsigned long int index = key_index((const unsigned char *)key, ht->size);
+    hash_node_t *current_node = ht->array[index];
 
-	/* Calculate the index for the given key using key_index function */
-	index = key_index((const unsigned char *)key, ht->size);
+    while (current_node != NULL)
+    {
+        if (strcmp(current_node->key, key) == 0)
+        {
+            free(current_node->value);
+            current_node->value = strdup(value);
+            return (current_node->value == NULL ? 0 : 1);
+        }
+        current_node = current_node->next;
+    }
 
-	/* Check if the key already exists in the hash table */
-	current_node = ht->array[index];
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			/* Key already exists, update the value and return 1 */
-			free(current_node->value); /* Free the old value */
-			current_node->value = strdup(value);
-			if (current_node->value == NULL)
-				return (0); /* Return 0 if strdup fails */
-			return (1);
-		}
-		current_node = current_node->next;
-	}
+    hash_node_t *new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
 
-	/* Key does not exist, create a new node and add it at the beginning of the list */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (0);
+    }
 
-	new_node->key = strdup(key); /* Duplicate the key */
-	if (new_node->key == NULL)
-	{
-		free(new_node); /* Free the new_node if strdup fails */
-		return (0);
-	}
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
 
-	new_node->value = strdup(value); /* Duplicate the value */
-	if (new_node->value == NULL)
-	{
-		free(new_node->key); /* Free the key if strdup fails */
-		free(new_node); /* Free the new_node if strdup fails */
-		return (0);
-	}
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
 
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-
-	return (1);
+    return (1);
 }
