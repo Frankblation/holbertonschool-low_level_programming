3fca070e5d547fb9434f7684c86a663d1bf5acce
Frankblation <frankblation@gmail.com>
2023-08-16 09:54:29 -0700

    betty fix 1

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,21 +2,21 @@
 /**
 * linear_search - matches elememnts of whole list.
 * @array: the array
-* @size_t:
+* @size: size of the number in array
 * @value: the key
 */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if(array == NULL)
-		return -1;
+		if (array == NULL)
+		return (-1);
 
-	for (i = 0; i < size; i++)
-	{
+		for (i = 0; i < size; i++)
+		{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
-	}
-	 return -1;
+			return (i);
+		}
+		return (-1);
 }
